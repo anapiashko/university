5afe768e819f72731371bfbbcaceceedefd72409
Anastasiya Piashko <anapiashko@gmail.com>
2020-10-14 01:22:23 +0300

    update RIPEMD128

--- KMZI/lab1_hash_function/src/Algorithm_RIPEMD128.java
+++ KMZI/lab1_hash_function/src/Algorithm_RIPEMD128.java
@@ -4,9 +4,7 @@ public class Algorithm_RIPEMD128 {
 
     private String message;
     private final long message_length;
-    private String[][] blocks;
     private byte[] byteMessage;
-    private byte[][] blocksByte;
 
     private final BigInteger[] K1 = { new BigInteger("00000000",16),
             new BigInteger("5A827999", 16),
@@ -25,30 +23,6 @@ public class Algorithm_RIPEMD128 {
             h2 = new BigInteger("98BADCFE", 16),
             h3 = new BigInteger("10325476", 16);
 
-    private final int[] R1 = {0, 1, 2, 3, 4,5 ,6,7 ,8, 9,10, 11, 12, 13, 14, 15,
-                        7, 4, 13, 1, 10,6 ,15,3 ,12, 0,9, 5, 2, 14, 11, 8,
-                        3, 10, 14, 4, 9, 15 ,8,1 ,2, 7,0, 6, 13, 11, 5, 12,
-                        1, 9, 11, 10, 0, 8 , 12, 4, 13, 3, 7, 15, 14, 5, 6, 2,
-                        4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13};
-
-    private final int[] R2 = {5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12,
-                        6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2,
-                        15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13,
-                        8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14,
-                        12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11};
-
-    private final int[] S1 = {11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
-                        7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
-                        11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
-                        11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
-                        9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6};
-
-    private final int[] S2 = {8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6,
-                        9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11,
-                        9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5,
-                        15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8,
-                        8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11};
-
     public Algorithm_RIPEMD128 (String message) {
         this.message = message;
         this.message_length = message.length();
@@ -64,9 +38,9 @@ public class Algorithm_RIPEMD128 {
     public void step_1_init() {
         // вместо init
         System.out.println("-----------bytes_meth------------");
-        int numberBlocks = (message.length() / Constants.BLOCK_SIZE_BYTE) + 1;
+        int numberBlocks = (message.length() /64) + 1;
 
-        byteMessage = new byte[numberBlocks * Constants.BLOCK_SIZE_BYTE];
+        byteMessage = new byte[numberBlocks * 64];
 
         byte[] temp = message.getBytes();
         for (int i = 0; i < message_length; i++) {
@@ -116,6 +90,12 @@ public class Algorithm_RIPEMD128 {
         }
     }
 
+    // Определение констант и используемых функций.
+    void step_3() {
+
+    }
+
+    // Основной цикл.
     void step_4() {
         for (int i = 0; i < byteMessage.length / 64; i++) {
             BigInteger A1 = h0, B1 = h1, C1 = h2, D1 = h3;
@@ -124,17 +104,12 @@ public class Algorithm_RIPEMD128 {
             BigInteger T;
             for (int j = 0; j < 64; j++) {
 
-                System.out.println("J = " + j +", BIN STRING = " + utfToBin(subArray(i,R1[j])));
-
                 T = ((A1
                         .add(function_f(j,B1,C1,D1))
-                                .add(utfToBin(subArray(i,R1[j])))
+                                .add(utfToBin(subArray(i,R1(j))))
                                 .add(getValueFromK(j, K1)))
                         .mod(BigInteger.valueOf(2).pow(32)));
-                System.out.println("T = " + T);
-                T = cyclicLeftShift(T, T.toString(2).length(), S1[j]);
-
-                System.out.println("T = " + T);
+                T = cyclicLeftShift(T, T.toString(2).length(), S1(j));
 
                 A1 = D1;
                 D1 = C1;
@@ -143,11 +118,11 @@ public class Algorithm_RIPEMD128 {
 
                 T = ((A2
                         .add(function_f(63 - j, B2, C2, D2))
-                        .add(utfToBin(subArray(i,R2[j])))
+                        .add(utfToBin(subArray(i,R2(j))))
                         .add(getValueFromK(j, K2)))
                         .mod(BigInteger.valueOf(2).pow(32)));
 
-                T = cyclicLeftShift(T, T.toString(2).length(), S2[j]);
+                T = cyclicLeftShift(T, T.toString(2).length(), S2(j));
 
                 A2 = D2;
                 D2 = C2;
@@ -155,11 +130,20 @@ public class Algorithm_RIPEMD128 {
                 B2 = T;
 
             }
+            System.out.println("A1 = " + A1);
+            System.out.println("B1 = " + B1);
+            System.out.println("C1 = " + C1);
+            System.out.println("D1 = " + D1);
 
-            T = h1.add(C1).add(D2);
-            h1 = h2.add(D1).add(A2);
-            h2 = h3.add(A1).add(B2);
-            h3 = h0.add(B1).add(C2);
+            System.out.println("A2 = " + A2);
+            System.out.println("B2 = " + B2);
+            System.out.println("C2 = " + C2);
+            System.out.println("D2 = " + D2);
+
+            T = (h1.add(C1).add(D2)).mod(BigInteger.valueOf(2).pow(32));
+            h1 = (h2.add(D1).add(A2)).mod(BigInteger.valueOf(2).pow(32));
+            h2 = (h3.add(A1).add(B2)).mod(BigInteger.valueOf(2).pow(32));
+            h3 = (h0.add(B1).add(C2)).mod(BigInteger.valueOf(2).pow(32));
             h0 = T;
         }
 
@@ -169,13 +153,14 @@ public class Algorithm_RIPEMD128 {
         System.out.println("h2 = " + h2);
         System.out.println("h3 = " + h3);
 
-//        System.out.println("------------16-----------");
-//        System.out.println("h0 = " + BigInteger.valueOf(h0));
-//        System.out.println("h1 = " + Integer.toHexString(h1));
-//        System.out.println("h2 = " + Integer.toHexString(h2));
-//        System.out.println("h3 = " + Integer.toHexString(h3));
+        System.out.println("------------16-----------");
+        System.out.println("h0 = " + h0.toString(16));
+        System.out.println("h1 = " + h1.toString(16));
+        System.out.println("h2 = " + h2.toString(16));
+        System.out.println("h3 = " + h3.toString(16));
     }
 
+    // i - номер блока, j - номер слова
     byte[] subArray(int i, int j){
         byte[] arr = new byte[4];
         arr[3] = byteMessage[i*64 + j*4];
@@ -185,11 +170,18 @@ public class Algorithm_RIPEMD128 {
         return arr;
     }
 
-    // Определение констант и используемых функций.
-    void step_3() {
-
+    BigInteger h32Bit(BigInteger h) {
+        if(h.toString(2).length() < 32) {
+            StringBuffer strB = new StringBuffer(h.toString(2));
+            while (strB.length() != 32) {
+                strB.insert(0, "0");
+            }
+            BigInteger bigInt = new BigInteger(strB.toString(), 2);
+            h = bigInt;
+            System.out.println("less : " + h.toString(2));
+        }
+        return h;
     }
-
     BigInteger utfToBin(byte[] utf) {
 
         // Convert to binary
@@ -225,7 +217,6 @@ public class Algorithm_RIPEMD128 {
 
     private BigInteger function_f(int j, BigInteger x, BigInteger y, BigInteger z) {
         if (j >= 0 && j <= 15) {
-            System.out.println("j = " + j + ", func_f() = " + x.xor(y).xor(z));
             return x.xor(y).xor(z);
         } else if (j >= 16 && j <= 31) {
             return (x.and(y)).or(x.not().and(z));
@@ -254,5 +245,43 @@ public class Algorithm_RIPEMD128 {
         } else {
             return BigInteger.valueOf(-1);
         }
+    }
+
+    private int R1(int j) {
+        int[][] R = {
+                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+                {7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8},
+                {3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12},
+                {1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2},
+                {4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13}};
+        return R[j / 16][j % 16];
+    }
+
+    private int R2(int j) {
+        int[][] R = {{5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12},
+                {6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2},
+                {15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13},
+                {8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14},
+                {12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11}};
+        return R[j / 16][j % 16];
+    }
+
+    private int S1(int j) {
+        int[][] S = {{11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8},
+                {7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12},
+                {11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5},
+                {11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12},
+                {9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6}};
+        return S[j / 16][j % 16];
+
+    }
+
+    private int S2(int j) {
+        int[][] S = {{8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6},
+                {9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11},
+                {9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5},
+                {15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8},
+                {8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11}};
+        return S[j / 16][j % 16];
     }
 }
--- KMZI/lab1_hash_function/src/MainClass.java
+++ KMZI/lab1_hash_function/src/MainClass.java
@@ -1,40 +1,18 @@
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class MainClass {
 
     public static void main(String[] args) {
-        String message = "Hello world";
+        String m = "Hello world";
         String test = "abc";
 
-        // ----First step-----
+        System.out.print("Input string : ");
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
 
-        BigInteger h0 = new BigInteger("67452301",16),
-                h1 = new BigInteger("EFCDAB89", 16),
-                h2 = new BigInteger("98BADCFE", 16),
-                h3 = new BigInteger("10325476", 16);
-
-        BigInteger b = new BigInteger("5619165283");
-
-        System.out.println(rotateLeft(new BigInteger("5619165283")));
-        System.out.println(cyclicLeftShift(new BigInteger("5619165283"),b.toString(2).length(),11));
-
-        String str = b.toString(2);
-        System.out.println(str.length());
-
-       Algorithm_RIPEMD128 algorithm_ripemd128 = new Algorithm_RIPEMD128(test);
-       algorithm_ripemd128.start();
-
-//        RIPEMD_128 algh = new RIPEMD_128();
-//        algh.Run(test);
-
-    }
-
-    private static BigInteger rotateLeft(BigInteger bi) {
-        BigInteger ret = bi.shiftLeft(11);
-        if (ret.testBit(32)) {
-            ret = ret.clearBit(32).setBit(0);
-        }
-        return ret;
+        Algorithm_RIPEMD128 algorithm_ripemd128 = new Algorithm_RIPEMD128(message);
+        algorithm_ripemd128.start();
     }
 
     static BigInteger allOnes(int L) {
