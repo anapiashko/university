6cada5bf454898c7f328ff5d4565eccbeb4b3c9b
Anastasiya Piashko <anapiashko@gmail.com>
2021-03-03 00:48:04 +0300

    add more exception handling lab2 YPIS

--- YPIS/lab2/src/Main.java
+++ YPIS/lab2/src/Main.java
@@ -1,17 +1,25 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String text = read();
         System.out.println("SOURCE TEXT : " + text);
-        if(!isStringSpellingCorrect(text)){
-           return;
+        if (!isStringSpellingCorrect(text)) {
+            return;
         }
         String[] words = text.split("[\\s.,(); ]+");
-       // System.out.println(Arrays.toString(words));
+        // System.out.println(Arrays.toString(words));
 
         words = deleteCommentWords(words);
+        int incorrectWord = correctWords(words);
+        if (incorrectWord >= 0) {
+            System.out.println("word \"" + words[incorrectWord] + "\" is not correct");
+            return;
+        }
         //System.out.println(Arrays.toString(words));
 //        //split(text, ";*:-+() ");
 
@@ -22,9 +30,9 @@ public class Main {
             char symbol = text.charAt(i);
 
             // комментарии оформляются в виде : ‘/* */’
-            if(symbol == '/' && text.charAt(i+1) == '*'){
-                while (true){
-                    if(text.charAt(i) == '*' && text.charAt(i+1) == '/'){
+            if (symbol == '/' && text.charAt(i + 1) == '*') {
+                while (true) {
+                    if (text.charAt(i) == '*' && text.charAt(i + 1) == '/') {
                         break;
                     }
                     i++;
@@ -32,7 +40,7 @@ public class Main {
                 i++;
             }
             // если слово начинается с буквы, то это имя процедуры, т.к. другие параметры заключены в ковычки
-            else if (symbol >= 97 && symbol <= 122) {
+            else if (symbol >= 97 && symbol <= 122 || symbol == '_') {
                 System.out.format("%-20s%s\n", words[wordCount], "Procedure name");
                 i += words[wordCount].length() - 1;
                 wordCount++;
@@ -92,6 +100,11 @@ public class Main {
             } else {
                 map.put(chars[i], map.get(chars[i]) + 1);
             }
+
+            if (chars[i] == ')' && chars[i + 1] != ';') {
+                System.out.println("symbol ; was expected!");
+                result = false;
+            }
         }
 
         if (!map.get('(').equals(map.get(')'))) {
@@ -107,20 +120,60 @@ public class Main {
             System.out.println("symbol " + '\'' + " was expected!");
             result = false;
         }
-        if(!correctComments(str)) {
+        if (!correctComments(str)) {
             System.out.println("comments is not correct!");
             result = false;
         }
         //System.out.println(map.toString());
         return result;
     }
+
+    // метод проверяет правильность имени процедур
+    // возвращает индекс неправильного слова
+    // или -1 в случае, если неправильное слово отсутсвует
+    static int correctWords(String[] words) {
+
+        // проверка первого символа названия процедуры (должна быть буква или нижнее подчеркивание)
+        int incorrectWord = -1;
+        for (int i = 0; i < words.length - 1; i++) {
+            String word = words[i];
+            char symbol = word.charAt(0);
+            if (symbol >= 97 && symbol <= 122 || symbol == '_' ||
+                    symbol == '\'' || symbol == '\"') {
+            } else {
+                incorrectWord = i;
+                return incorrectWord;
+            }
+        }
+
+        // проверка на наличие запрещенных символов в середине слова
+        for (int j = 0; j < words.length; j++) {
+            String word = words[j];
+            for (int i = 1; i < word.length(); i++) {
+                char symbol = word.charAt(i);
+
+                if (symbol != '\'' && symbol != '\"') {
+                    if (symbol >= 97 && symbol <= 122 || symbol == '_' ||
+                            symbol >= 48 && symbol <= 57 || symbol >= 65 && symbol <= 90) {
+
+                    } else {
+                        System.out.println("symbol " + symbol + " is unresolved in this context!");
+                        incorrectWord = j;
+                        return incorrectWord;
+                    }
+                }
+            }
+        }
+        return incorrectWord;
+    }
+
     static boolean correctComments(String source) {
         boolean commentOpen = true;
         for (int i = 0; i < source.length() - 1; i++) {
-            if(source.charAt(i) == '/' && source.charAt(i+1) == '*'){
+            if (source.charAt(i) == '/' && source.charAt(i + 1) == '*') {
                 commentOpen = true;
             }
-            if(source.charAt(i) == '*' && source.charAt(i+1) == '/' && commentOpen){
+            if (source.charAt(i) == '*' && source.charAt(i + 1) == '/' && commentOpen) {
                 commentOpen = false;
             }
         }
@@ -132,16 +185,16 @@ public class Main {
         int start = 0, end = 0;
         List<String> list = new LinkedList<>(Arrays.asList(words));
         for (int i = 0; i < words.length; i++) {
-            if(list.get(i).charAt(0) == '/'&& list.get(i).charAt(1) == '*'){
+            if (list.get(i).charAt(0) == '/' && list.get(i).charAt(1) == '*') {
                 start = i;
             }
-            if(list.get(i).charAt(list.get(i).length() - 2) == '*'&& list.get(i).charAt(list.get(i).length() - 1) == '/'){
+            if (list.get(i).charAt(list.get(i).length() - 2) == '*' && list.get(i).charAt(list.get(i).length() - 1) == '/') {
                 end = i;
             }
         }
 
-        list.subList(start, end+1).clear();
-        words = list.toArray(words);
+        list.subList(start, end + 1).clear();
+        words = list.toArray(new String[0]);
         return words;
     }
 }
