2a656a14c483b67110284275bbf7d9a789f95f37
anapiashko <anapiashko@gmail.com>
2021-09-17 16:40:16 +0300

    CCP added lab1 task1

---
+++ CCP/lab1/src/color/column/Shape.java
@@ -0,0 +1,18 @@
+package color.column;
+
+public enum Shape {
+
+    TRIANGLE("Triangle"),
+    SQUARE("Square"),
+    CIRCLE("Circle");
+
+    private String name;
+
+    Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
